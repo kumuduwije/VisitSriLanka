b4a0e04ee22d8d830659395230d2006d5e61f060
Kumudu Wijewardhana <kumuduwijewardana3@gmail.com>
2022-11-19 23:03:36 +0530

    Initial Commit

---
+++ VisitSriLanka/VisitSriLankaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitSriLankaApp.swift
+//  VisitSriLanka
+//
+//  Created by Kumudu Wijewardhana on 2022-11-19.
+//
+
+import SwiftUI
+
+@main
+struct VisitSriLankaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
